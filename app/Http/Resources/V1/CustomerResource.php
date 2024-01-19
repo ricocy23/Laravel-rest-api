e911089179a42809dc595cdf1dbd97332781500d
ricocy23 <ricousuyatno123@students.amikom.ac.id>
2024-01-19 07:34:04 +0700

    commit

--- app/Http/Resources/V1/CustomerResource.php
+++ app/Http/Resources/V1/CustomerResource.php
@@ -15,7 +15,14 @@ class CustomerResource extends JsonResource
     public function toArray(Request $request): array
     {
         return [
-            'id' =>$this->id
+            'id' =>$this->id,
+            'name' => $this->name,
+            'type'=> $this->type,
+            'email' => $this->email,
+            'address' =>$this->address,
+            'city' => $this->city,
+            'state' => $this->state,
+            'postalCode' => $this->postal_code
         ];
     }
 }
